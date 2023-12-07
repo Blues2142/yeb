0dfe4665dadc1299cc868bce0e8af032ad3af3f2
Administrator <Administrator@DESKTOP-IVHCABN>
2023-12-07 11:20:46 +0800

    公共返回对象

---
+++ yeb-server/src/main/java/com/xxxx/server/service/IEmployeeRemoveService.java
@@ -0,0 +1,16 @@
+package com.xxxx.server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xxxx.server.pojo.EmployeeRemove;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 2142
+ * @since 2023-12-01
+ */
+public interface IEmployeeRemoveService extends IService<EmployeeRemove> {
+
+}
---
+++ yeb-server/src/main/java/com/xxxx/server/service/IOplogService.java
@@ -0,0 +1,16 @@
+package com.xxxx.server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xxxx.server.pojo.Oplog;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 2142
+ * @since 2023-12-01
+ */
+public interface IOplogService extends IService<Oplog> {
+
+}
